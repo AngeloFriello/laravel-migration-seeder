6b69b4db8539cae8dd6defe4b8d9975be895e41b
angelo friello <angelofriello01@gmail.com>
2023-12-13 15:37:07 +0100

    add trains

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="container">
+        <ul class="d-flex gap-4">
+            <li>clicca qui</li>
+            
+        </ul>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <ul class="d-flex gap-3">
+                    <li>PAGINA PROVA</li>
+                    <li>movies</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
